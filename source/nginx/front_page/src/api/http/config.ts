5a03ecdb698c284935d658b45fdf663690d62411
Phillweston <2436559745@qq.com>
2022-06-18 16:34:42 +0800

    Update Script files

--- source/nginx/front_page/src/api/http/config.ts
+++ source/nginx/front_page/src/api/http/config.ts
@@ -3,13 +3,13 @@ export const CURRENT_CONFIG = {
   baseURL: 'http://localhost:6789/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
   websocketURL: 'ws://localhost:6789/api/v1/ws',  // Example: 'ws://192.168.1.1:6789/api/v1/ws'
 
-  rtmpURL: 'rtmp://localhost/live/',  // Example: 'rtmp://192.168.1.1/live/'
+  rtmpURL: 'Please enter the rtmp access address.',  // Example: 'rtmp://192.168.1.1/live/'
   gb28181Para:
     'serverIP=Please enter the server ip.&serverPort=Please enter the server port.&serverID=Please enter the server id.' + 
     '&agentID=Please enter the agent id.&agentPassword=Please enter the agent password' + 
     '&localPort=Please enter the local port.&channel=Please enter the channel.',
   rtspPara: 'userName=Please enter the username.&password=Please enter the password&port=Please enter the port.',
-  amapKey: 'Please enter the amap key.',
+  amapKey: '9808aa00b8408faade58e9a7ad34c6cd',
   agoraAPPID: 'Please enter the agora app id.',
   agoraToken: 'Please enter the agora token.',
   agoraChannel: 'Please enter the agora channel.',
